a53f731d5addaea3be0d9ab3c16f1b2ca286f98f
Igor Korsunov <elquendi@yandex.ru>
2021-03-24 00:10:42 +0300

    Save checkpoints according to the validation mAP

--- chest_xray/cli/train.py
+++ chest_xray/cli/train.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 from omegaconf import OmegaConf
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from pytorch_lightning.utilities import rank_zero_only
 
 from .hydra import init_hydra, parse_hydra_config
 from ..data.datamodule import XRayDataModule
@@ -30,14 +32,22 @@ def run() -> None:
     if args.print_final_config:
         print(OmegaConf.to_yaml(config, resolve=True))
 
-    trainer = Trainer.from_argparse_args(args)
+    checkpoints = ModelCheckpoint(monitor='mAP', filename='{epoch}_{mAP:.3f}', save_top_k=5, mode='max')
+    trainer = Trainer.from_argparse_args(args, callbalcks=[checkpoints, LearningRateMonitor()])
     data = XRayDataModule(config.data)
     model = instantiate_model(config.model.num_classes, config.model.trainable_backbone_layers)
     experiment = Experiment(model, config)
     trainer.fit(experiment, datamodule=data)
+    report_checkpoints(checkpoints)
     predictions = trainer.predict(experiment, datamodule=data)
     parsed_predictions = parse_predictions(predictions, data.predict_dataset.image_ids)
     generate_submission_file(config.submission.file, parsed_predictions, config.evaluation.confidence_threshold)
+
+
+@rank_zero_only
+def report_checkpoints(checkpointing_callback: ModelCheckpoint) -> None:
+    print(f'Best mAP was {checkpointing_callback.best_model_score}')
+    print(f'Path to the best checkpoint is {checkpointing_callback.best_model_path}')
 
 
 if __name__ == '__main__':
